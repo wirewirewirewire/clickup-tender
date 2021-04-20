2fc5fc762185108ba7017615c496abd123768988
Robert Gühne <robert@polyxostudios.de>
2021-04-21 00:07:58 +0200

    fix(Stations): added delay

--- components/DescriptionAdvanced/index.js
+++ components/DescriptionAdvanced/index.js
@@ -34,16 +34,20 @@ function Image({ e, next }) {
   return <img src={e.insert.image} className={styles.image} />;
 }
 
-export default function DescriptionAdvanced({ children, id }) {
+export default function DescriptionAdvanced({ id, delay }) {
   const taskInclude =
     "?include_groups=true&fields%5B%5D=content&fields%5B%5D=assignees&fields%5B%5D=dependencies&fields%5B%5D=parent_task&fields%5B%5D=subtask_parent_task&fields%5B%5D=attachments&fields%5B%5D=hidden_attachments&fields%5B%5D=followers&fields%5B%5D=totalTimeSpent&fields%5B%5D=subtasks&fields%5B%5D=todoComments&fields%5B%5D=mentions&fields%5B%5D=tags&fields%5B%5D=position&fields%5B%5D=simple_statuses&fields%5B%5D=viewing&fields%5B%5D=commenting&fields%5B%5D=customFields&fields%5B%5D=statuses&fields%5B%5D=members&fields%5B%5D=features&fields%5B%5D=rolledUpTimeSpent&fields%5B%5D=rolledUpTimeEstimate&fields%5B%5D=rolledUpPointsEstimate&fields%5B%5D=views&fields%5B%5D=linkedTasks&fields%5B%5D=last_viewed&fields%5B%5D=new_thread_count&fields%5B%5D=commit_counts&fields%5B%5D=relationships&markItemViewed=true&include_archived_subtasks=true";
 
   const [responseAdvanced, loadingAdvanced, hasErrorAdvanced] = useFetch(
-    `http://localhost:8002/v1/task/${id}${taskInclude}`
+    `http://localhost:8002/v1/task/${id}${taskInclude}`,
+    delay
   );
+  x;
 
-  if (!responseAdvanced) return <span>Loading {id}</span>;
-  console.log("responseAdvanced", JSON.parse(responseAdvanced.content));
+  if (!responseAdvanced?.content)
+    return (
+      <span className={styles.loading}>Loading DescriptionAdvanced {id}</span>
+    );
 
   const content = JSON.parse(responseAdvanced.content);
   if (!content) return null;
